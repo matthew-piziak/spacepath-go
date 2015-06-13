92b17b5cc355f3a7e23e5e316ba8152d59a40d9a
Matthew Piziak <matt@Matthews-MacBook-Air.local>
2015-06-13 17:19:32 -0400

    implemented basic A*

---
+++ path.go
@@ -0,0 +1,73 @@
+// Generalized pathing functions
+package spacepath
+
+import (
+	"fmt"
+	"math"
+)
+
+type Node struct {
+	x     int8
+	y     int8
+	vx    int8
+	vy    int8
+	angle uint8
+}
+
+func min(open map[Node]float64) Node {
+	waterline := math.Inf(1)
+	best := Node{x: 0, y: 0, vx: 0, vy: 0, angle: 0}
+	for k, v := range open {
+		if v < waterline {
+			waterline = v
+			best = k
+		}
+	}
+	return best
+}
+
+func reconstructPath(cameFrom map[Node]Node, node Node) []Node {
+	if _, ok := cameFrom[node]; ok {
+		path := append(reconstructPath(cameFrom, cameFrom[node]), node)
+		return path
+	}
+	return []Node{node}
+}
+
+// A* pathing algorithm
+func AStar(
+	start Node,
+	goal Node,
+	adjacent func(Node) []Node,
+	heuristic func(Node, Node) float64,
+	success func(Node, Node) bool) []Node {
+	seen := make(map[Node]bool)
+	open := make(map[Node]float64)
+	cameFrom := make(map[Node]Node)
+	gScore := make(map[Node]float64)
+	fScore := make(map[Node]float64)
+	gScore[start] = 0
+	fScore[start] = gScore[start] + heuristic(start, goal)
+	open[start] = fScore[start]
+	seen[start] = true
+	for {
+		node := min(open)
+		delete(open, node)
+		fmt.Printf("node: %+v\n", node)
+		if success(node, goal) {
+			return reconstructPath(cameFrom, node)
+		}
+		for _, adj := range adjacent(node) {
+			if seen[adj] {
+				continue
+			}
+			seen[adj] = true
+			cameFrom[adj] = node
+			// adjacency is based on a constant time step
+			gScore[adj] = gScore[node] + 1
+			hScore := heuristic(adj, goal)
+			fScore[adj] = gScore[adj] + hScore
+			open[adj] = fScore[adj]
+		}
+	}
+}
---
+++ path_test.go
@@ -0,0 +1,37 @@
+package spacepath
+
+import (
+	"math"
+	"testing"
+)
+
+func manhattan(node Node) []Node {
+	adjacencyList := []Node{
+		Node{node.x + 1, node.y, node.vx, node.vy, node.angle},
+		Node{node.x - 1, node.y, node.vx, node.vy, node.angle},
+		Node{node.x, node.y + 1, node.vx, node.vy, node.angle},
+		Node{node.x, node.y - 1, node.vx, node.vy, node.angle}}
+	return adjacencyList
+}
+
+func euclideanDistance(Node Node, goal Node) float64 {
+	return math.Sqrt(
+		math.Pow(float64(goal.x)-float64(Node.x), 2) +
+			math.Pow(float64(goal.y)-float64(Node.y), 2))
+}
+
+func nodeEqual(Node Node, goal Node) bool {
+	return Node == goal
+}
+
+func TestAStar(t *testing.T) {
+	start := Node{x: 0, y: 0, vx: 0, vy: 0, angle: 0}
+	goal := Node{x: 4, y: 4, vx: 0, vy: 0, angle: 0}
+	adjacent := manhattan
+	heuristic := euclideanDistance
+	success := nodeEqual
+	path := AStar(start, goal, adjacent, heuristic, success)
+	if len(path) != 9 {
+		t.Errorf("Expected length 9, was length %d.", len(path))
+	}
+}
